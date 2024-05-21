ba7ce8fbe49fedfa9b483c072e8bc65a4c138ea5
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-05-21 16:45:00 +0200

    first commit

---
+++ backend/src/main/java/com/workingschedule/WorkingScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.workingschedule.workingschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkingScheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkingScheduleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/workingschedule/workingschedule/WorkingScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workingschedule.workingschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkingScheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
