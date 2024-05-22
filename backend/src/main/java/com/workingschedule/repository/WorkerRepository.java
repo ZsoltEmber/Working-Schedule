3f5e50184aac44c56e3841ece46d86a371ad61d1
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-05-22 13:59:05 +0200

    fix packaging problem

--- backend/src/main/java/com/workingschedule/repository/WorkerRepository.java
+++ backend/src/main/java/com/workingschedule/repository/WorkerRepository.java
@@ -1,14 +1,14 @@
 package com.workingschedule.repository;
 
 import com.workingschedule.model.Worker;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 
+@Repository
 public interface WorkerRepository extends JpaRepository<Worker, Long> {
 
-    @Override
-    List<Worker> findAll();
 }
+
+
+
