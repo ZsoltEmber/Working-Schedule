78560c7b8d109cfa0bc877071ad9f1278e21b7e2
ZsoltEmber <132936073+ZsoltEmber@users.noreply.github.com>
2024-06-04 11:34:52 +0200

    add login mechanic

--- backend/src/main/java/com/workingschedule/controller/UserController.java
+++ backend/src/main/java/com/workingschedule/controller/UserController.java
@@ -1,52 +1,69 @@
 package com.workingschedule.controller;
 
 import com.workingschedule.model.AppUser;
+import com.workingschedule.model.payload.JwtResponse;
+import com.workingschedule.model.payload.LoginRequest;
 import com.workingschedule.model.payload.RegisterUserRequest;
-import com.workingschedule.repository.UserRepository;
+import com.workingschedule.security.Role;
 import com.workingschedule.security.jwt.JwtUtils;
 import com.workingschedule.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
-    private final UserRepository userRepository;
     private final JwtUtils jwtUtils;
-    private UserService userService;
+    private final UserService userService;
+    private final AuthenticationManager authenticationManager;
+
 
     @Autowired
-    public UserController(UserRepository userRepository, JwtUtils jwtUtils, UserService userService) {
-        this.userRepository = userRepository;
+    public UserController(JwtUtils jwtUtils, UserService userService, AuthenticationManager authenticationManager) {
         this.jwtUtils = jwtUtils;
         this.userService = userService;
+        this.authenticationManager = authenticationManager;
     }
 
 
+    //CREATE
     @PostMapping("/register")
-    AppUser registerUser (@RequestBody RegisterUserRequest registerUserRequest){
+    AppUser registerUser(@RequestBody RegisterUserRequest registerUserRequest) {
         return userService.registerUser(registerUserRequest);
     }
 
-    //CREATE
-//    @PostMapping("/register")
-//    public User registerUser(@RequestBody RegisterUserRequest registerUserRequest) {
-//    }
 
-//    @PostMapping("/login")
-//    public JwtResponse authenticateUser(@RequestBody LoginRequest loginRequest) {
-//        Authentication authentication = authenticationManager
-//                .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.name(), loginRequest.password()));
-//
-//        SecurityContextHolder.getContext().setAuthentication(authentication);
-//        String jwt = jwtUtils.generateToken(authentication);
-//        org.springframework.security.core.userdetails.User userDetails = (org.springframework.security.core.userdetails.User) authentication.getPrincipal();
-//        List<String> roles = userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority)
-//                .toList();
-//
-//        return new JwtResponse(jwt, userDetails.getUsername(), roles);
-//
-//    }
+    @PostMapping("/login")
+    public ResponseEntity<JwtResponse> logIn(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.username(), loginRequest.password()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateToken(authentication);
+        System.out.println(jwt);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
+        Collection<? extends GrantedAuthority> authorities = userDetails.getAuthorities();
+
+        for (GrantedAuthority authority : authorities) {
+            if (authority.getAuthority().equals(Role.ROLE_USER.name())) {
+                return ResponseEntity
+                        .ok(new JwtResponse(jwt, userDetails.getUsername(), List.of(Role.ROLE_USER)));
+            }
+        }
+
+        throw new EntityNotFoundException("User not found.");
+    }
 }
--- backend/src/main/java/com/workingschedule/model/payload/JwtResponse.java
+++ backend/src/main/java/com/workingschedule/model/payload/JwtResponse.java
@@ -1,6 +1,8 @@
 package com.workingschedule.model.payload;
 
+import com.workingschedule.security.Role;
+
 import java.util.List;
 
-public record JwtResponse(String jwt, String userName, List<String> roles) {
+public record JwtResponse(String jwt, String userName, List<Role> roles) {
 }
